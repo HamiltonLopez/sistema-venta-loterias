c07ebecefb05bca1ae02fa80ddd490e8b802dbbc
Hamilton Lopez <hamilt727@gmail.com>
2025-11-09 12:55:17 -0500

    feat(frontend): mejorar lista de sorteos con navegación - Mejorada interfaz de usuario con botones de navegación

--- frontend/src/app/features/sorteos/sorteo-list/sorteos-list.ts
+++ frontend/src/app/features/sorteos/sorteo-list/sorteos-list.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { SorteoService, Sorteo } from '../../../core/services/sorteo.service';
 import { NgFor } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
-
+import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-sorteos-list',
   standalone: true,
-  imports: [NgFor, MatCardModule],
+  imports: [NgFor, MatCardModule,RouterLink, MatIconModule],
   templateUrl: './sorteos-list.html',
   styleUrls: ['./sorteos-list.scss']
 })
