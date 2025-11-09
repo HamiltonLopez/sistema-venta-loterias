26d3ee260d17010a363e2a02c83d541c15b19bde
Hamilton Lopez <hamilt727@gmail.com>
2025-11-09 12:54:45 -0500

    feat(frontend): integrar navbar en componente principal
    - Agregado NavbarComponent al App component
    - Cambiado template para incluir barra de navegación

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NavbarComponent } from './shared/navbar/navbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  imports: [RouterOutlet, NavbarComponent,MatToolbarModule, MatIconModule],
+  template:  `
+    <app-navbar></app-navbar>
+    <main class="content">
+      <router-outlet></router-outlet>
+    </main>
+  `,
   styleUrl: './app.scss'
 })
 export class App {
